dc6e2639f22a044b89ab2e0133be60facd33d037
KIM <skok1025@naver.com>
2020-06-21 13:25:46 +0900

    front boot

--- ApartFriend_frontend/src/main/java/com/twoplus/apartfriend/BootApp.java
+++ ApartFriend_frontend/src/main/java/com/twoplus/apartfriend/BootApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // no database setting
+// front boot
 public class BootApp {
 	
 	public static void main(String[] args) {
