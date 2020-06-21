2739590107055d27690da6ef4ab9d43537529f8c
KIM <skok1025@naver.com>
2020-06-21 13:51:25 +0900

    lombok vo 적용

--- ApartFriend_backend/src/main/java/com/twoplus/apartfriend/vo/TestVo.java
+++ ApartFriend_backend/src/main/java/com/twoplus/apartfriend/vo/TestVo.java
@@ -5,6 +5,13 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class TestVo {
 
 	@Pattern(regexp = "^[0-9]*$",message = "숫자만 입력")
@@ -13,30 +20,6 @@ public class TestVo {
 	@NotEmpty
 	@Length(max = 20)
 	private String testString;
-	
-	
-
-	public String getTestString() {
-		return testString;
-	}
-
-	public void setTestString(String testString) {
-		this.testString = testString;
-	}
-
-	public Integer getTestFieldName() {
-		return testFieldName;
-	}
-
-	public void setTestFieldName(Integer testFieldName) {
-		this.testFieldName = testFieldName;
-	}
-
-	@Override
-	public String toString() {
-		return "TestVo [testFieldName=" + testFieldName + ", testString=" + testString + "]";
-	}
-
 	
 
 }
